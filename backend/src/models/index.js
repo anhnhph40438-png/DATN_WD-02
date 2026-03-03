966170f2009e43d96306409fd54f0a427c651c16
anhnhph40438-png <anhnhph40438@fpt.edu.vn>
2026-03-03 14:46:10 +0700

    update index

---
+++ backend/src/models/index.js
@@ -0,0 +1,19 @@
+const User = require('./User');
+const Shop = require('./Shop');
+const Service = require('./Service');
+const Barber = require('./Barber');
+const Appointment = require('./Appointment');
+const Review = require('./Review');
+const Transaction = require('./Transaction');
+const Promotion = require('./Promotion');
+
+module.exports = {
+  User,
+  Shop,
+  Service,
+  Barber,
+  Appointment,
+  Review,
+  Transaction,
+  Promotion
+};
