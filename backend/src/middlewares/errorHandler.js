692c09f5befdffddd51ffaaf3106871aeee834b9
atuan2810a <atuan28102004a@gmail.com>
2026-03-05 21:42:43 +0700

    update errorHandler

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -54,3 +54,38 @@ const sendErrorProd = (err, res) => {
     });
   }
 };
+
+const errorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else {
+    let error = { ...err };
+    error.message = err.message;
+    error.name = err.name;
+
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(err);
+    }
+
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(err);
+    }
+
+    if (err.name === 'ValidationError') {
+      error = handleValidationErrorDB(err);
+    }
+
+    if (err.name === 'JsonWebTokenError') {
+      error = handleJWTError();
+    }
+
+    if (err.name === 'TokenExpiredError') {
+      error = handleJWTExpiredError();
+    }
+
+    sendErrorProd(error, res);
+  }
+};
