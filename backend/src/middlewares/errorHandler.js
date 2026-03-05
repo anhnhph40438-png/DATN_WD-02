8ff900e1e85eb55e36393467081933a4e9724b9c
atuan2810a <atuan28102004a@gmail.com>
2026-03-05 21:41:08 +0700

    update handleValidationErrorDB

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -5,3 +5,9 @@ const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
