e6db6a92fc1859739b1e6a697e4671c95d08ed41
Vinhnhph40784 <150607368+Vinhnhph40784@users.noreply.github.com>
2026-03-19 17:59:38 +0700

    index admin

---
+++ frontend/src/pages/admin/index.js
@@ -0,0 +1,10 @@
+// Admin Pages - Dashboard, Users, Barbers, Services, Appointments, Settings, Promotions, Transactions, Statistics
+export { default as AdminDashboard } from './AdminDashboard';
+export { default as AdminUsers } from './AdminUsers';
+export { default as AdminBarbers } from './AdminBarbers';
+export { default as AdminServices } from './AdminServices';
+export { default as AdminAppointments } from './AdminAppointments';
+export { default as AdminPromotions } from './AdminPromotions';
+export { default as AdminTransactions } from './AdminTransactions';
+export { default as AdminStatistics } from './AdminStatistics';
+export { default as AdminSettings } from './AdminSettings';
