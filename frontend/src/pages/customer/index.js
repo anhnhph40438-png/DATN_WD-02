28630a4496c849f8d34160eb2541953370142456
Đào Tuân <dao2005tuan@gmail.com>
2026-04-04 22:20:16 +0700

    giao diện khách hàng

---
+++ frontend/src/pages/customer/index.js
@@ -0,0 +1,10 @@
+// Customer Pages - Home, Services, Barbers, Booking, Appointments, Profile, Payment
+export { default as HomePage } from './HomePage';
+export { default as ServicesPage } from './ServicesPage';
+export { default as BarbersPage } from './BarbersPage';
+export { default as BarberDetailPage } from './BarberDetailPage';
+export { default as BookingPage } from './BookingPage';
+export { default as MyAppointmentsPage } from './MyAppointmentsPage';
+export { default as ProfilePage } from './ProfilePage';
+export { default as PaymentPage } from './PaymentPage';
+export { default as PaymentResultPage } from './PaymentResultPage';
